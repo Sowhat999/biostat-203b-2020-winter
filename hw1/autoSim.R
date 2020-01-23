601f6ae946ec9f54c5b4866f40b443e0480da05a
bursontung97 <bursontung97@gmail.com>
2020-01-23 02:08:08 +0000

    error occurs when using system function

---
+++ hw1/autoSim.R
@@ -0,0 +1,20 @@
+# autoSim.R
+
+# set sample size and distribution type vector
+nVals <- seq(100, 500, by=100)
+distTypes = c("gaussian", "t5", "t1")
+
+# run runSim.R with each sample size and type
+for (n in nVals) {    # loop for sample sizes
+  for (Dt in distTypes){    # loop for distribution types
+    
+    oFile <- paste(Dt,"_n", n, ".txt", sep="")
+    sysCall <- paste("nohup Rscript runSim.R n=", n, " Dt=\"", Dt,"\" rep=", 50," seed=", 203," > ", oFile, sep="")
+    print(class(sysCall))
+    system(sysCall, wait = TRUE)
+    print(paste("sysCall=", sysCall, sep=""))
+    
+  }
+}
+
+
