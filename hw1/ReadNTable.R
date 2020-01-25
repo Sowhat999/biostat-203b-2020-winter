64051fa26796056d11042c0ffa64cf4e0f1bb680
bursontung97 <bursontung97@gmail.com>
2020-01-25 03:48:24 +0000

    Fixed bug

--- hw1/ReadNTable.R
+++ hw1/ReadNTable.R
@@ -6,7 +6,7 @@ nVals <- seq(100, 500, by=100)
 distTypes = c("gaussian", "t5", "t1")
 
 # print the title
-cat(sprintf('\n\n%5s%10s%10s%10s%10s\n','n','Method','Gaussian','t_5','t_1'))
+cat(sprintf('\n\n%5s%10s%10s%10s%12s\n','n','Method','Gaussian','t_5','t_1'))
 
 # Read txt files with each sample size and type (9 combinations)
 for (n in nVals) {    # loop for sample sizes
@@ -23,6 +23,6 @@ for (n in nVals) {    # loop for sample sizes
     prtTb[col_n] = newTb[2]
     col_n = col_n + 1
   }
-  cat(sprintf('%5d%10s%10.3f%10.3f%10.3f\n', n, 'PrimeAvg', prtTb[1,1], prtTb[1,2], prtTb[1,3]))
-  cat(sprintf('%5d%10s%10.3f%10.3f%10.3f\n', n, 'SampAvg', prtTb[1,1], prtTb[1,2], prtTb[1,3]))
+  cat(sprintf('%5d%10s%10.6f%10.6f%12.6f\n', n, 'PrimeAvg', prtTb[1,1], prtTb[1,2], prtTb[1,3]))
+  cat(sprintf('%5d%10s%10.6f%10.6f%12.6f\n', n, 'SampAvg', prtTb[2,1], prtTb[2,2], prtTb[2,3]))
 }
