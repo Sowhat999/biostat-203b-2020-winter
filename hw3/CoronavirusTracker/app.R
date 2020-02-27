c4aed9080ba50d143d8d7d284f1a8883180c3fd1
bursontung97 <bursontung97@biostat-203b-2020winter-20200218.c.biostat-203b-2020winter.internal>
2020-02-27 18:45:52 +0000

    tabPanel used, mainPanel not working

--- hw3/CoronavirusTracker/app.R
+++ hw3/CoronavirusTracker/app.R
@@ -10,40 +10,76 @@
 library(shiny)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-
+ui <- navbarPage(
+    title = "Conronavirus Visualization App",
     # # Application title
-    # titlePanel("Map and Trend of Coronavirus"),
+    
     # 
     # wellPanel(
     #     dateInput('date',
     #               label = 'Date input: yyyy-mm-dd',
     #               value = Sys.Date()
     #     ))
+    tabPanel(title = "China",
     
-    column(4, wellPanel(
-        
+    titlePanel("Map and Trend of Coronavirus"),
+    fluidRow(column(3, wellPanel(
+
         dateInput('date',
                   label = 'Date to display',
                   value = Sys.Date() ),
-        
-        dateInput('date2',
-                  label = paste('Date input 2: string for starting value,',
-                                'dd/mm/yy format, locale ja, range limited,',
-                                'week starts on day 1 (Monday)'),
-                  value = as.character(Sys.Date()),
-                  min = Sys.Date() - 5, max = Sys.Date() + 5,
-                  format = "dd/mm/yy",
-                  startview = 'year', language = 'zh-TW', weekstart = 1),
-        
-        selectInput('Case', label = "Case to display for the map", 
-                    choices = c("Confirmed" = "Confirmed", 
-                                "Death" = "Death", 
-                                "Recovered" = "Recovered")),
-        tableOutput("case")
 
-        ) )
+        # dateInput('date2',
+        #           label = paste('Date input 2: string for starting value,',
+        #                         'dd/mm/yy format, locale ja, range limited,',
+        #                         'week starts on day 1 (Monday)'),
+        #           value = as.character(Sys.Date()),
+        #           min = Sys.Date() - 5, max = Sys.Date() + 5,
+        #           format = "dd/mm/yy",
+        #           startview = 'year', language = 'zh-TW', weekstart = 1),
+
+        selectInput('Case', label = "Case to display for the map",
+                    choices = c("Confirmed" = "Confirmed",
+                                "Death" = "Death",
+                                "Recovered" = "Recovered")),
+        tableOutput("Case")
+
+        )),
+
+        column(width = 8, mainPanel(plotOutput("distPlot")))
+        )
+    ),
+    
+    tabPanel(title = "World",
+        titlePanel("Map and Trend of Coronavirus"),
+        fluidRow(column(3, wellPanel(
+
+            dateInput('date',
+                      label = 'Date to display',
+                      value = Sys.Date() ),
+
+            # dateInput('date2',
+            #           label = paste('Date input 2: string for starting value,',
+            #                         'dd/mm/yy format, locale ja, range limited,',
+            #                         'week starts on day 1 (Monday)'),
+            #           value = as.character(Sys.Date()),
+            #           min = Sys.Date() - 5, max = Sys.Date() + 5,
+            #           format = "dd/mm/yy",
+            #           startview = 'year', language = 'zh-TW', weekstart = 1),
+
+            selectInput('Case', label = "Case to display for the map",
+                        choices = c("Confirmed" = "Confirmed",
+                                    "Death" = "Death",
+                                    "Recovered" = "Recovered")),
+            tableOutput("Case")
+
+        )),
+
+        column(width = 8, offset = 2, plotOutput("distPlot"))
+        )
+    )
 )
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
@@ -51,7 +87,7 @@ server <- function(input, output) {
     output$distPlot <- renderPlot({
         # generate bins based on input$bins from ui.R
         x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        bins <- seq(min(x), max(x), length.out = 1)
 
         # draw the histogram with the specified number of bins
         hist(x, breaks = bins, col = 'darkgray', border = 'white')
